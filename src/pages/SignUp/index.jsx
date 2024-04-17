b494ae525a359663f1129e2967b3b92830d0b720
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-04-17 19:48:58 +0100

    Config AXIOS - connection create account API

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react"
 import { FiLock, FiMail, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from "../../services/api"
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
@@ -7,6 +10,32 @@ import { Input } from '../../components/Input'
 import { Background, Container, Form } from './styles'
 
 export function SignUp() {
+  const [ name, setName ] = useState("")
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert ("Please, fill in all the fields")
+    }
+
+    api.post("/users", { name, email, password })
+    .then(()=>{
+      alert ("User successfully registered")
+      navigate("/")
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message)
+      }else {
+        alert("Something went wrong")
+      }
+    })
+
+
+  }
   return (
     <Container>
       <Background />
@@ -21,21 +50,24 @@ export function SignUp() {
           placeholder="Name"
           type="text"
           icon={FiUser}
+          onChange={e=> setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e=> setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange={e=> setPassword(e.target.value)}
         />
 
-        <Button title="Sign up" />
+        <Button title="Sign up" onClick={handleSignUp} />
 
         <Link to="/" >Back to login</Link>
       </Form>
