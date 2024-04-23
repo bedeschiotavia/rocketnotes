5b78d690e0330f552be0864561e21b9b4b07c78a
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-04-23 11:17:39 +0100

    Routes redirect

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 
 import { FiLock, FiMail } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
@@ -8,6 +11,16 @@ import { Input } from '../../components/Input'
 import { Background, Container, Form } from './styles'
 
 export function SignIn() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useAuth()
+
+  function handleSignIn (){
+    signIn({ email, password })
+  }
+  
   return (
     <Container>
       <Form>
@@ -20,15 +33,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange= {e=> setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange= {e=> setPassword(e.target.value)}
         />
 
-        <Button title="Login" />
+        <Button title="Login" onClick={handleSignIn} />
 
         <Link to="/register">Register</Link>
       </Form>
